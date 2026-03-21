34243cc2f5d7fd9f6a87e120554925e7bd19d9d3
Sahan Munasingha <edu.sahan@gmail.com>
2026-03-21 12:47:38 +0530

    add separate mongoDB databases for for each service

--- healthcare -platform/services/ai-symptom-service/src/main/java/com/healthcare/ai_symptom_service/AiSymptomServiceApplication.java
+++ healthcare -platform/services/ai-symptom-service/src/main/java/com/healthcare/ai_symptom_service/AiSymptomServiceApplication.java
@@ -1,5 +1,6 @@
 package com.healthcare.ai_symptom_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiSymptomServiceApplication {
 
 	public static void main(String[] args) {
+
+		// Load .env file first
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(AiSymptomServiceApplication.class, args);
 	}
 
