97647c71ea0afd0019821c8057e07315f91cad57
Sahan Munasingha <edu.sahan@gmail.com>
2026-03-22 23:12:57 +0530

    update user authentication functionality

--- healthcare -platform/services/auth-service/src/main/java/com/healthcare/auth_service/dto/RegisterRequest.java
+++ healthcare -platform/services/auth-service/src/main/java/com/healthcare/auth_service/dto/RegisterRequest.java
@@ -1,12 +1,23 @@
 package com.healthcare.auth_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "Role is required")
     private String role; // PATIENT, DOCTOR, ADMIN
 
     public void setName(String name) {
